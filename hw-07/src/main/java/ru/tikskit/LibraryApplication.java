abb8bda2247dae0e833905552570e72ec593f8ef
tikskit <tikskit@gmail.com>
2021-07-06 10:43:59 +0700

    migration system set up

--- hw-07/src/main/java/ru/tikskit/LibraryApplication.java
+++ hw-07/src/main/java/ru/tikskit/LibraryApplication.java
@@ -3,6 +3,12 @@ package ru.tikskit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// url h2 консоли: http://localhost:8080/h2-console
+// url базы: jdbc:h2:mem:librarydb
+
+
+
+
 @SpringBootApplication
 public class LibraryApplication {
 
