3e807288000fc2a859a53d8d7f51ac679dc57cd6
tikskit <tikskit@gmail.com>
2021-08-03 11:30:39 +0700

    Project starget from scratch

---
+++ hw-13/src/main/java/ru/tikskit/LibraryMongoApplication.java
@@ -0,0 +1,13 @@
+package ru.tikskit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryMongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryMongoApplication.class, args);
+	}
+
+}
---
+++ hw-13/src/test/java/ru/tikskit/LibraryMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.tikskit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryMongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
