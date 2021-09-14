516038bd3ef0432196a8e1d2108010ecfc664102
tikskit <tikskit@gmail.com>
2021-09-14 11:44:55 +0700

    init files added

---
+++ hw-16/src/main/java/ru/tikskit/MvcViewApplication.java
@@ -0,0 +1,13 @@
+package ru.tikskit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcViewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvcViewApplication.class, args);
+	}
+
+}
