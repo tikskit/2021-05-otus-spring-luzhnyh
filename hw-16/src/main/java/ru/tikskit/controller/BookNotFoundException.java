533a75afd7dc517fcba58ef556ff44984681148e
tikskit <tikskit@gmail.com>
2021-09-14 21:04:13 +0700

    ResponseStatus for BookNotFoundException was added

--- hw-16/src/main/java/ru/tikskit/controller/BookNotFoundException.java
+++ hw-16/src/main/java/ru/tikskit/controller/BookNotFoundException.java
@@ -1,5 +1,9 @@
 package ru.tikskit.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "No such book found")
 public class BookNotFoundException extends RuntimeException {
     public BookNotFoundException() {
     }
