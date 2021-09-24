ba155a5f1df77dda100acbfdacd06024055ae436
tikskit <tikskit@gmail.com>
2021-09-24 20:03:00 +0700

    1. removed unused exception classes 2. removed unused html files

--- hw-17/src/main/java/ru/tikskit/controller/GlobalControllerExceptionHandler.java
+++ hw-17/src/main/java/ru/tikskit/controller/GlobalControllerExceptionHandler.java
@@ -7,22 +7,6 @@ import org.springframework.web.servlet.ModelAndView;
 @ControllerAdvice
 public class GlobalControllerExceptionHandler {
 
-    @ExceptionHandler(AuthorNotFoundException.class)
-    public ModelAndView handleAuthorNotFoundException(AuthorNotFoundException e) {
-        ModelAndView modelAndView = new ModelAndView("entitynotfounderror");
-        modelAndView.addObject("title", "Автор не найден!");
-        modelAndView.addObject("text", "Автор не найден!");
-        return modelAndView;
-    }
-
-    @ExceptionHandler(GenreNotFoundException.class)
-    public ModelAndView handleAuthorNotFoundException(GenreNotFoundException e) {
-        ModelAndView modelAndView = new ModelAndView("entitynotfounderror");
-        modelAndView.addObject("title", "Жанр не найден!");
-        modelAndView.addObject("text", "Жанр не найден!");
-        return modelAndView;
-    }
-
     @ExceptionHandler(RuntimeException.class)
     public ModelAndView handleRuntimeException(RuntimeException e) {
         ModelAndView modelAndView = new ModelAndView("error");
@@ -31,11 +15,4 @@ public class GlobalControllerExceptionHandler {
         return modelAndView;
     }
 
-    @ExceptionHandler(BookCrudException.class)
-    public ModelAndView handleBookCrudException(BookCrudException e) {
-        ModelAndView modelAndView = new ModelAndView("bookerror");
-        modelAndView.addObject("title", "Ошибка при работе с книгой!");
-        modelAndView.addObject("text", String.format("Произошла ошибка: %s", e.getMessage()));
-        return modelAndView;
-    }
 }
