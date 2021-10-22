c376490972ffbd6b9bc605cfbd49ef5373ff4cfa
tikskit <tikskit@gmail.com>
2021-10-22 10:55:58 +0700

    Security beans configurations moved to SecurityConfiguration

--- hw-25/src/main/java/ru/tikskit/AuthApplication.java
+++ hw-25/src/main/java/ru/tikskit/AuthApplication.java
@@ -1,11 +1,7 @@
 package ru.tikskit;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.crypto.password.PasswordEncoder;
-
-import javax.annotation.PostConstruct;
 
 // url h2 консоли: http://localhost:8080/h2-console
 // url базы: jdbc:h2:mem:librarydb
--- hw-25/src/main/java/ru/tikskit/security/SecurityConfiguration.java
+++ hw-25/src/main/java/ru/tikskit/security/SecurityConfiguration.java
@@ -1,12 +1,25 @@
 package ru.tikskit.security;
 
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.security.SecureRandom;
 
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    private static final int strength = 10;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+    @Autowired
+    private DatabaseUserDetailsService userDetailsService;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -15,6 +28,19 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .authorizeRequests().antMatchers("/**").authenticated()
                 .and()
                 .formLogin();
+    }
+
+    @Bean
+    public AuthenticationProvider daoAuthenticationProvider() {
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder);
+        daoAuthenticationProvider.setUserDetailsService(userDetailsService);
+        return daoAuthenticationProvider;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(strength, new SecureRandom());
     }
 
 }
