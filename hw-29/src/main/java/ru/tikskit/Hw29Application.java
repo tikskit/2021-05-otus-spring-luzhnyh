2a8f8d607f352c1295aef8668165db9d1237ee08
tikskit <tikskit@gmail.com>
2021-11-10 10:46:14 +0700

    init files added

---
+++ hw-29/src/main/java/ru/tikskit/Hw29Application.java
@@ -0,0 +1,13 @@
+package ru.tikskit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hw29Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hw29Application.class, args);
+	}
+
+}
