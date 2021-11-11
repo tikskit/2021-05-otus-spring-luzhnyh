f439c9eee8e5225cb14f53e7b0fcf8c4cc96e38e
tikskit <tikskit@gmail.com>
2021-11-11 20:03:15 +0700

    bean and method name is using instead of direct call

--- hw-29/src/main/java/ru/tikskit/config/Channels.java
+++ hw-29/src/main/java/ru/tikskit/config/Channels.java
@@ -8,10 +8,6 @@ import org.springframework.integration.dsl.IntegrationFlow;
 import org.springframework.integration.dsl.IntegrationFlows;
 import org.springframework.integration.dsl.Transformers;
 import ru.tikskit.config.dto.ProductChanges;
-import ru.tikskit.config.dto.ProductDto;
-import ru.tikskit.service.BrandNewProductService;
-import ru.tikskit.service.InStockAgainService;
-import ru.tikskit.service.OnlyFewLeftService;
 import ru.tikskit.service.ProductChangesPromoAnalyst;
 
 import java.util.concurrent.Executors;
@@ -22,9 +18,6 @@ import java.util.concurrent.Executors;
 public class Channels {
 
     private final ProductChangesPromoAnalyst promoAnalyst;
-    private final InStockAgainService inStockAgainService;
-    private final BrandNewProductService brandNewProductService;
-    private final OnlyFewLeftService onlyFewLeftService;
 
     /**
      * Тут конфигурируем бизнес-логику обработки изменения товара на остатке
@@ -40,21 +33,19 @@ public class Channels {
                         c.subscribe(sf -> sf
                                 .filter(promoAnalyst::isInStockAgain)
                                 .transform(Transformers.converter(ProductChanges::getCurProduct))
-                                .handle(m ->
-                                        inStockAgainService.sendNotification((ProductDto) m.getPayload())
-                                )
+                                .handle("inStockAgainServiceImpl", "sendNotification")
                         )
                 )
                 .publishSubscribeChannel(c -> // обрабатываем случай, когда товар новый
                         c.subscribe(sf -> sf
                                 .filter(promoAnalyst::isNewProduct)
                                 .transform(Transformers.converter(ProductChanges::getCurProduct))
-                                .handle(m -> brandNewProductService.addProduct((ProductDto) m.getPayload()))))
+                                .handle("brandNewProductServiceImpl", "addProduct")))
                 .publishSubscribeChannel(c -> // обрабатываем случай, когда товара осталось немного
                         c.subscribe(sf -> sf
                                 .filter(promoAnalyst::isFewLeft)
                                 .transform(Transformers.converter(ProductChanges::getCurProduct))
-                                .handle(m -> onlyFewLeftService.notifyClients((ProductDto) m.getPayload()))))
+                                .handle("onlyFewLeftServiceImpl", "notifyClients")))
                 .get();
     }
 }
