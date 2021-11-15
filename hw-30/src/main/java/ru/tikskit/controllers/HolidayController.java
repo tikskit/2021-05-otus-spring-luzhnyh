a060fe446665f378ce4cbe076ae8d0435248b7d0
tikskit <tikskit@gmail.com>
2021-11-15 13:40:10 +0700

    Forgotten to include in prev commit

--- hw-30/src/main/java/ru/tikskit/controllers/HolidayController.java
+++ hw-30/src/main/java/ru/tikskit/controllers/HolidayController.java
@@ -1,10 +1,8 @@
 package ru.tikskit.controllers;
 
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.server.EntityLinks;
 import org.springframework.hateoas.server.ExposesResourceFor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
