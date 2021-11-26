174069feee499a68abc1ea32a5f8a128be4efafb
tikskit <tikskit@gmail.com>
2021-11-26 13:42:11 +0700

    minor changes

--- hw-35/src/main/java/ru/tikskit/HystrixApplication.java
+++ hw-35/src/main/java/ru/tikskit/HystrixApplication.java
@@ -3,17 +3,13 @@ package ru.tikskit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
-import org.springframework.context.ConfigurableApplicationContext;
-import ru.tikskit.dao.AuthorDao;
 
 @SpringBootApplication
 @EnableCircuitBreaker
 public class HystrixApplication {
 
 	public static void main(String[] args) {
-		ConfigurableApplicationContext context = SpringApplication.run(HystrixApplication.class, args);
-		AuthorDao authorDao = (AuthorDao) context.getBean("authorDao");
-		System.out.println(authorDao);
+		SpringApplication.run(HystrixApplication.class, args);
 	}
 
 }
